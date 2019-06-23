5e91628e9ae0b456a1410c78a39bbdc1b9dcc3e4
Enacheam <mr.acheampongsnr@gmail.com>
2019-06-22 22:47:43 -0500

    first commit

---
+++ src/main/java/com/kenacheam/studies/asedemo/AseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kenacheam.studies.asedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenacheam/studies/asedemo/AseDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kenacheam.studies.asedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AseDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
